77f36a3757e6b9513d2973f0526d036390cbe683
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-11-21 09:59:26 +0800

    modify header

--- dashboard.php
+++ dashboard.php
@@ -1,25 +1,4 @@
 <?php include('layout/header.php');?>
-<div id="layoutSidenav">
-    <div id="layoutSidenav_nav">
-        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-            <div class="sb-sidenav-menu">
-                <div class="nav">
-                    <a class="nav-link" href="index.html">
-                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                        Dashboard
-                    </a>
-                    <a class="nav-link" href="index.html">
-                        <div class="sb-nav-link-icon"><i class="fas fa-list-alt"></i></div>
-                        Inventory
-                    </a>
-                    <a class="nav-link" href="index.html">
-                        <div class="sb-nav-link-icon"><i class="fas fa-directions"></i></div>
-                        Stock Movement
-                    </a>
-                </div>
-            </div>
-        </nav>
-    </div>
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid px-4">
